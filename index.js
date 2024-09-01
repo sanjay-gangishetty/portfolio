b00801b95f6e0cf8a0002c01a7ff6a0011f4b524
Sanjay <gangishettysanjaykumar@gmail.com>
2024-09-01 12:52:35 +0530

    Implemented light/dark mode on resume page.

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ document.onscroll = function (e) {
     navMenu.classList.remove("active");
   }
 };
+
+// Toggles the theme
+const themeToggle = document.getElementById('theme-toggle');
+
+function setTheme(theme) {
+  if (theme === 'light') {
+    document.body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light');
+  } else {
+    document.body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark');
+  }
+}
+
+const savedTheme = localStorage.getItem('theme') || 'dark';
+setTheme(savedTheme);
+
+themeToggle.addEventListener('click', () => {
+  if (document.body.classList.contains('light-theme')) {
+    setTheme('dark');
+  } else {
+    setTheme('light');
+  }
+});
